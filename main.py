0a09b028d9e45bedf5d168a010d0c83259ef6441
Kikinchuk Vlad <114030086+SauroFan@users.noreply.github.com>
2022-12-20 12:38:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,280 @@
+x = input("Введіть дані - ").lower()
+
+vld = len(x)
+
+vladlist = list(x)
+
+def a():
+    print('''  ### 
+ #   #
+ #   #
+ #####
+ #   #
+ #   #
+ #   #\n''' )
+def b():
+        print('''#### 
+#   #
+#   #
+#### 
+#   #
+#   #
+####\n''' )
+def c():
+    print(''' ### 
+#   #
+#  
+# 
+#
+#   #
+ ### \n''' )
+def d():
+    print('''#### 
+#   #
+#   #
+#   #
+#   #
+#   #
+####\n''' )
+def e():
+    print('''#####
+#
+#
+###
+#
+#
+#####\n''' )
+def f():
+    print('''#####
+#
+#
+###
+#
+#
+#\n''' )
+def g():
+    print(''' ###
+#   #
+#
+#  ##
+#   #
+#   #
+ ###\n''' )
+def h():
+    print('''#   #
+#   #
+#   #
+#####
+#   #
+#   #
+#   #
+#   #\n''' )
+def i():
+    print('''###
+ #
+ #
+ #
+ #
+ #
+###\n''' )
+def j():
+    print('''###
+ #
+ #
+ #
+ #
+ #
+##\n''')
+def k():
+    print('''#   #
+#  #
+# #
+##
+# #
+#  #
+#   #\n''' )
+def l():
+    print('''#
+#
+#
+#
+#
+#
+#####\n''')
+def m():
+    print('''#   #
+#   #
+## ##
+# # #
+#   #
+#   #
+#   #\n''' )
+def n():
+    print('''#   #
+#   #
+##  #
+# # #
+#  ##
+#   #
+#   #\n''' )
+def o():
+    print(''' ### 
+#   #
+#   #
+#   #
+#   #
+#   #
+ ###\n''' )
+def p():
+    print('''####
+#   #
+#   #
+####
+#
+#
+#\n''' )
+def q():
+    print('''  ### 
+#   #
+#   #
+#   #
+#   #
+#   #
+#  # 
+ ## #\n''' )
+def r():
+    print('''#### 
+#   #
+#   #
+####
+# #
+#  
+#   #\n''' )
+def s():
+    print(''' ####
+#
+#
+ ###
+    #
+    #
+ ###\n''' )
+def t():
+    print('''#####
+  #
+  #
+  #
+  #
+  #
+  #\n''' )
+def u():
+    print('''#   #
+#   #
+#   #
+#   #
+#   #
+#   #
+ # #
+  #\n''' )
+def v():
+    print('''#   #
+#   #
+#   #
+#   #
+#   #
+#   #
+ # #
+  #\n''' )
+def w():
+    print('''#   #
+#   #
+#   #
+# # #
+## ##
+#   #
+#   #\n''' )
+def i():
+    print('''#   #
+#   #
+ # #
+  #
+ # #
+#   #
+#   #\n''' )
+def y():
+    print('''#   #
+#   #
+ # #
+  #
+  #
+  #
+  #\n''' )
+def z():
+    print('''#####
+    #
+   #
+  #
+ #
+#
+##### \n''' )
+
+
+for i in range(0, vld):
+    if(vladlist[i] == "a"):
+        a()
+    elif(vladlist[i] == "b"):
+        b()
+    elif(vladlist[i] == "c"):
+        c()
+    elif(vladlist[i] == "d"):
+        d()
+    elif(vladlist[i] == "e"):
+        e()
+    elif(vladlist[i] == "f"):
+        f()
+    elif(vladlist[i] == "g"):
+        g()
+    elif(vladlist[i] == "h"):
+        h()
+    elif(vladlist[i] == "i"):
+        i()
+    elif(vladlist[i] == "j"):
+        j()
+    elif(vladlist[i] == "k"):
+        k()
+    elif(vladlist[i] == "l"):
+        l()
+    elif(vladlist[i] == "m"):
+        m()
+    elif(vladlist[i] == "n"):
+        n()
+    elif(vladlist[i] == "o"):
+        o()
+    elif(vladlist[i] == "p"):
+        p()
+    elif(vladlist[i] == "q"):
+        q()
+    elif(vladlist[i] == "r"):
+        r()
+    elif(vladlist[i] == "s"):
+        s()
+    elif(vladlist[i] == "t"):
+        t()
+    elif(vladlist[i] == "u"):
+        u()
+    elif(vladlist[i] == "v"):
+        v()
+    elif(vladlist[i] == "w"):
+        w()
+    elif(vladlist[i] == "i"):
+        i()
+    elif(vladlist[i] == "y"):
+        y()
+    elif(vladlist[i] == "z"):
+        z()
+
+
+
+
+
+
+
